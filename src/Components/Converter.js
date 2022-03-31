9dbf9d89cf555cf25a76283756257ac3c2af8ec9
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2022-03-31 18:14:09 +0200

    added function for data fetching, switch currencies, and convert currencies

--- src/Components/Converter.js
+++ src/Components/Converter.js
@@ -1,12 +1,43 @@
-import { useState } from 'react';
-import Dropdown from 'react-dropdown';
+import { useState, useEffect } from 'react';
+import Axios from "axios"
 import switcher  from '../images/Switcher.png'
 import logo from "../images/hatch-logo.png"
 import '../App.css';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 function Converter() {
   
-  const [options, setOptions] = useState([]);
+  const [options, setOptions] = useState([])
+  const [input , setInput ] = useState(0)
+  const [info, setInfo] = useState([]);
+  const [from, setFrom] = useState("eur");
+  const [to, setTo] = useState("usd");
+  const [output, setOutput] = useState(0);
+
+  useEffect(() => {
+    Axios.get(
+`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
+   .then((res) => {
+      setInfo(res.data[from]);
+    })
+  }, [from]);
+
+  useEffect(() => {
+    setOptions(Object.keys(info))
+    convert()
+  }, [info])
+
+  function convert() {
+    const rate = info[to]
+    setOutput(input * rate)
+  }
+
+  function flip() {
+    const temp = from
+    setFrom(to)
+    setTo(temp)
+  }
 
   return (
     <div className="converter">
@@ -22,33 +53,37 @@ function Converter() {
             <input 
              type="text" 
              placeholder="Enter amount" 
+             onChange={(e) => setInput(e.target.value)}
             />
         </div>
         <div className="from">
           <label>From</label>
           <Dropdown 
             options={options} 
-            value=""
-            placeholder="From" />
+            placeholder="From"
+            onChange={(e) => { setFrom(e.value)}}
+            value={from}
+         />
         </div>
         <div className="switch">
-          <img src={switcher} alt="switch" />
+          <img src={switcher} alt="switch" onClick={() => { flip()}} />
         </div>
         <div className="to">
           <label>To</label>
           <Dropdown 
             options={options} 
-            value="" 
-            placeholder="To" 
+            placeholder="To"
+            onChange={(e) => { setTo(e.value)}}
+            value={to}
         />
         </div>
         <div>
-            <button>Convert</button>
+            <button onClick={()=>{convert()}}>Convert</button>
         </div>
       </div>
       <div className='result'>
-          <p>1 EUR =</p>
-          <p>1.123445 USD</p>
+          <p>{input+" "+from+" = " }</p>
+          <p>{output.toFixed(2) + " " + to}</p>
       </div>
       <p>View conversion history</p>
     </div>
